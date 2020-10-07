41d0baa0f9a81ccfa7977a6ca28db4e6efc63df5
SUP3Rpanzy <72160734+SUP3Rpanzy@users.noreply.github.com>
2020-10-07 22:07:17 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
-$.getJSON(
+  /*$.getJSON(
     "https://api.openweathermap.org/data/2.5/weather?q=Potchefstroom&units=metric&appid=26ee4d6b109498c61f293b17bbca24f4", 
     funtion(data),{
 
-       /* var icon="https://openweathermap.org/img/wn/" + data.weather.icon + ".png",
+        var icon="https://openweathermap.org/img/wn/" + data.weather.icon + ".png",
         var temp= data.main.temp,
         var weather = data.weather.main,
 
